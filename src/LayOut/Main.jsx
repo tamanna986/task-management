08c35f300f6485ad05c950370cf7bc5af251c04d
tamanna986 <tamanna.986jahan@gmail.com>
2023-12-22 11:53:22 +0600

    review added

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,6 +1,7 @@
 
 import { Outlet } from "react-router-dom";
 import Navbar from "../Component/Navbar/Navbar";
+import Footer from "../Component/Navbar/Footer/Footer";
 
 
 const Main = () => {
@@ -8,6 +9,7 @@ const Main = () => {
         <div >
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Review/Review.jsx
+++ src/Pages/Review/Review.jsx
@@ -1,10 +1,44 @@
-
+import { useEffect, useState } from "react";
+import { CiStar } from "react-icons/ci";
 
 const Review = () => {
+
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() =>{
+    fetch('review.json')
+    .then(res => res.json())
+    .then(data => setReviews(data))
+  },[])
+
+  console.log
     return (
-        <div>
-          <h1>rvs</h1>  
+   <div className="ml-6 ">
+            <div className="items-center  grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 gap-5 my-20">
+          {
+            reviews.map( review =>
+              <div key={review.id} className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+        <div className="avatar">
+  <div className="w-24 rounded-full">
+    <img src={review.img} />
+  </div>
+</div>
+         
+          <div className="flex gap-10">
+          <h2 className="card-title">{review.username}</h2>
+            <p>{review.date}</p>
+         </div>
+         <p>{review.comment}</p>
+          <div className="card-actions w-full">
+            <button className="btn bg-orange-600 text-white w-full">{review.rating} <CiStar /></button>
+          </div>
         </div>
+      </div>
+              )
+          }
+        </div>
+   </div>
     );
 };
 
